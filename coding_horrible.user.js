79cffa62a5f56d190fbfefdde1a17bcb4895b5e0
Zachary Danger <zacharydangercampbell@gmail.com>
2009-05-20 09:47:30 -0500

    Rewriting everything to use jQuery. And adding script to change archive links to be random xkcd comics.

--- coding_horrible.user.js
+++ coding_horrible.user.js
@@ -7,15 +7,32 @@
 // @include http://*.codinghorror.com/*
 // ==/UserScript==
 
+// Add jQuery (borrowed from http://joanpiedra.com/jquery/greasemonkey/)
+var GM_JQ = document.createElement('script');
+GM_JQ.src = 'http://jquery.com/src/jquery-latest.js';
+GM_JQ.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(GM_JQ);
 
-function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
+// Check if jQuery's loaded
+function GM_wait() {
+ 	if(typeof unsafeWindow.jQuery == 'undefined') {
+		window.setTimeout(GM_wait,100);
+	} else {
+		$ = unsafeWindow.jQuery;
+		letsJQuery();
+	}
 }
+GM_wait();
 
-addGlobalStyle('* { font-weight: normal ! important; }'); //take care of Jeff's random bold fetish
+// All your GM code must be inside this function
+function letsJQuery() {
+	//override Jeff's random bold fetish
+	$("*").css('font-weight', 'normal');
+
+	//replace all links to coding horror archives with links to random xkcd comics. :]
+	$("a:regex(href, *codinghorror.com/blog/archives/*)").each(function() {
+		var new_url = 'http://xkcd.com/';
+		var random_number = Math.ceil(Math.random()*586);
+		$(this).attr('href', new_url + random_number);
+	});
+}
